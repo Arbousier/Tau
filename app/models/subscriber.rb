0e9e4a591bee7a5083b424a510d569b5e3359144
mcansky <mcansky@arbousier.info>
2011-03-25 00:47:54 +0100

    adding some stats

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -2,6 +2,16 @@ class Subscriber < ActiveRecord::Base
   validates_uniqueness_of :email
   validates_format_of :email, :with => %r{^(?:[_a-z0-9-]+)(\.[_a-z0-9-]+)*@([a-z0-9-]+)(\.[a-zA-Z0-9\-\.]+)*(\.[a-z]{2,4})$}i
 
+  scope :permanent, lambda { 
+          where("subscribers.interest = ?", Settings.interests[2])
+      }
+  scope :short, lambda { 
+          where("subscribers.interest = ?", Settings.interests[0])
+      }
+  scope :medium, lambda { 
+          where("subscribers.interest = ?", Settings.interests[1])
+      }
+
   def name
     return "#{self.firstname} #{self.lastname}"
   end
